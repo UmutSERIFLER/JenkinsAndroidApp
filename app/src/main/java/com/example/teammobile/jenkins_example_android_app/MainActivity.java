a04e031a4ecbadb011fb33bd9a35e62a4649b0a0
Team Mobile <teammobile@Teams-Mac-mini.local>
2018-10-04 15:35:52 +0200

    first commit

---
+++ app/src/main/java/com/example/teammobile/jenkins_example_android_app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.teammobile.jenkins_example_android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Test
+    public void subscription_isCorrect() throws Exception{
+        //We intentionally made the string null
+
+        String myName = null;
+
+        assertNull(myName);
+
+    }
+
+}
